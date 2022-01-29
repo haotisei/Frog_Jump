b367b8ebb28c6ba31615d5fb7056a1999d43ec08
haotisei <96281130+haotisei@users.noreply.github.com>
2022-01-29 20:31:19 +0900

    camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform CamTransform;
+    public Transform Player;
+    public Vector3 offset;
+    public float followspeed;
+
+    void Start()
+    {
+        CamTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        Vector3 targetPosition = new Vector3(Player.position.x, CamTransform.position.y, CamTransform.position.z);
+
+        CamTransform.position = Vector3.Lerp(CamTransform.position, targetPosition + offset, Time.deltaTime * followspeed);
+    }
+}
